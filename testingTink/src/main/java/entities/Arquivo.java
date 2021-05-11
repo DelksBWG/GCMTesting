aaf866895da04d07165ba8d01466271e129f4975
DelksBWG <83842762+DelksBWG@users.noreply.github.com>
2021-05-11 10:45:59 -0300

    Add files via upload

---
+++ testingTink/src/main/java/entities/Arquivo.java
@@ -0,0 +1,27 @@
+package entities;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Arquivo {
+	private FileInputStream Aceito;
+	private File Arquivo;
+	public Arquivo(File arquivo) throws FileNotFoundException {
+		setAceito(new FileInputStream(arquivo.getAbsolutePath()));
+		setArquivo(arquivo);
+	}
+	public FileInputStream getAceito() {
+		return Aceito;
+	}
+	public void setAceito(FileInputStream conteudoArquivo) {
+		this.Aceito = conteudoArquivo;
+	}
+	public File getArquivo() {
+		return Arquivo;
+	}
+	public void setArquivo(File arquivo) {
+		Arquivo = arquivo;
+	}
+
+}
---
+++ testingTink/src/main/java/util/ProcessosCompletos.java
@@ -0,0 +1,104 @@
+package util;
+import java.io.File;
+import java.util.Scanner;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.IvParameterSpec;
+
+import entities.*;
+public class ProcessosCompletos {
+public static void Criptacao(Scanner sc) {
+	String caminho = new String(AdquirindoArquivo.ListandoEAdquirindo(sc));
+	try {
+	Arquivo arquivoRecebido = new Arquivo(new File(caminho));
+    SecretKey chave = Conversao.GerandoChave();
+    ChaveCodificar chaveFinal = new ChaveCodificar(chave);
+    String chaveString = Conversao.ChaveParaString(chaveFinal.getChave());
+    System.out.println(chaveString);
+    IvParameterSpec ivParameterSpec = Conversao.GerandoIv();
+    VetorInicializador vi = new VetorInicializador(ivParameterSpec);
+    String ivString = Conversao.ByteParaString(vi.getIv().getIV());
+    System.out.println(ivString);
+    String algoritmo = "AES/GCM/NoPadding";
+    GCMParameterSpec gcmParameterSpec = Conversao.GerandoGCM(vi.getIv().getIV());
+    ChaveGCM gcm = new ChaveGCM(gcmParameterSpec);
+    ArquivoCriptografado resultado = new ArquivoCriptografado(new File(caminho));	 
+    PegandoArquivo.adquirindo(chaveFinal, gcm.getGcmTag(), algoritmo, arquivoRecebido, resultado);
+    PegandoArquivo.imprimindo(resultado.getArquivo());
+}
+catch(Exception e) {
+System.out.println(caminho);
+sc.close();
+}
+}
+public static void CriptacaoString(Scanner sc) {
+	try {
+		System.out.print("Me conte seu segredo ");
+	String texto = sc.nextLine();
+    SecretKey chave = Conversao.GerandoChave();
+    ChaveCodificar chaveFinal = new ChaveCodificar(chave);
+    String chaveString = Conversao.ChaveParaString(chaveFinal.getChave());
+    System.out.println(chaveString);
+    IvParameterSpec ivParameterSpec = Conversao.GerandoIv();
+    VetorInicializador vi = new VetorInicializador(ivParameterSpec);
+    String ivString = Conversao.ByteParaString(vi.getIv().getIV());
+    System.out.println(ivString);
+    String algoritmo = "AES/GCM/NoPadding";
+    GCMParameterSpec gcmParameterSpec = Conversao.GerandoGCM(vi.getIv().getIV());
+    ChaveGCM gcm = new ChaveGCM(gcmParameterSpec);
+    String textoCriptografado = PegandoArquivo.adquirindoString(chaveFinal, gcm.getGcmTag(), algoritmo, texto);
+    System.out.println("Resultado : " + textoCriptografado);
+}
+catch(Exception e) {
+sc.close();
+}
+}
+public static void Decriptacao(Scanner sc) {
+	String caminho = new String(AdquirindoArquivo.ListandoEAdquirindo(sc));
+	try {
+		sc.nextLine();
+	Arquivo arquivoRecebido = new Arquivo(new File(caminho));
+    SecretKey chave = Conversao.StringParaChave(sc.nextLine());
+    ChaveCodificar chaveFinal = new ChaveCodificar(chave);
+    String chaveString = Conversao.ChaveParaString(chaveFinal.getChave());
+    System.out.println(chaveString);
+    IvParameterSpec ivParameterSpec = new IvParameterSpec(Conversao.StringParaByte(sc.nextLine()));
+    VetorInicializador vi = new VetorInicializador(ivParameterSpec);
+    String ivString = Conversao.ByteParaString(vi.getIv().getIV());
+    System.out.println(ivString);
+    String algoritmo = "AES/GCM/NoPadding";
+    GCMParameterSpec gcmParameterSpec = Conversao.GerandoGCM(vi.getIv().getIV());
+    ChaveGCM gcm = new ChaveGCM(gcmParameterSpec);
+    ArquivoCriptografado resultado = new ArquivoCriptografado(new File(caminho));
+    PegandoArquivo.retornando(chaveFinal, gcm.getGcmTag(), algoritmo, arquivoRecebido, resultado);
+    PegandoArquivo.imprimindo(resultado.getArquivo());
+}
+	catch(Exception e) {
+		System.out.println(caminho);
+		sc.close();
+		}
+}
+public static void DecriptacaoString(Scanner sc) {
+	try {
+		System.out.print("Me conte seu segredo ");
+	String texto = sc.nextLine();
+    SecretKey chave = Conversao.StringParaChave(sc.nextLine());
+    ChaveCodificar chaveFinal = new ChaveCodificar(chave);
+    String chaveString = Conversao.ChaveParaString(chaveFinal.getChave());
+    System.out.println(chaveString);
+    IvParameterSpec ivParameterSpec = new IvParameterSpec(Conversao.StringParaByte(sc.nextLine()));
+    VetorInicializador vi = new VetorInicializador(ivParameterSpec);
+    String ivString = Conversao.ByteParaString(vi.getIv().getIV());
+    System.out.println(ivString);
+    String algoritmo = "AES/GCM/NoPadding";
+    GCMParameterSpec gcmParameterSpec = Conversao.GerandoGCM(vi.getIv().getIV());
+    ChaveGCM gcm = new ChaveGCM(gcmParameterSpec);
+    String textoDecriptografado = PegandoArquivo.retornandoString(chaveFinal, gcm.getGcmTag(), algoritmo, texto);
+    System.out.println("Resultado : " + textoDecriptografado);
+}
+catch(Exception e) {
+sc.close();
+}
+}
+}
